c5c310de71058fb764ad89374428c91a8d443a1a
Ubaid Rana <ubaidrana45@gmail.com>
2020-05-15 19:05:25 +0200

    Initial check-in of module MVVM

---
+++ MVVM/Views/PlaylistDetailPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmDemo;
+using Xamarin.Forms;
+
+namespace MVVM.Views
+{
+    public partial class PlaylistDetailPage : ContentPage
+    {
+        private Playlist _playlist;
+
+        public PlaylistDetailPage(Playlist playlist)
+        {
+            _playlist = playlist;
+
+            InitializeComponent();
+
+            title.Text = _playlist.Title;
+        }
+    }
+}
